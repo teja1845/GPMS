dda804fa8b47ff216d16d36bf718a3f546689996
teja1845 <tejasree.angalakuduru@gmail.com>
2025-02-27 10:06:20 +0530

    Final Home display

--- firstapp/views.py
+++ firstapp/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.template import loader
-from django.shortcuts import render,request,redirect
+from django.shortcuts import render,redirect
 
 def add():
     print('hello')
