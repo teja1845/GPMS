bdce900695d56f372e6c4ad3d16decc1114fb67f
teja1845 <tejasree.angalakuduru@gmail.com>
2025-02-27 09:57:21 +0530

    Homepage display

--- firstapp/views.py
+++ firstapp/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render,request,redirect
 
 def add():
     print('hello')
 
+
+def home(request):
+    request.session['flag'] = 0
+    template = loader.get_template("home.html")
+    context = {'flag' : request.session['flag']}
+    return HttpResponse(template.render(context,request))
 
 # Create your views here.
